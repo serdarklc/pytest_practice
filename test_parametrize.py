105f2abc59965c2d6f114e194824b0f45c4cc36d
Serdar KLC <serdarklc@192.168.1.9>
2025-05-11 20:40:23 +0300

    parametrize was implemented

--- test_parametrize.py
+++ test_parametrize.py
@@ -1,5 +1,5 @@
 import pytest
 
-@pytest.mark.parametrize("a,b,expected", [(2,3,5),(4,5,9),(10,20,90)])
+@pytest.mark.parametrize("a,b,expected", [(2,3,5),(4,5,9),(10,20,30)])
 def test_sum(a,b,expected):
     assert a+b == expected
